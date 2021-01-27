895e89b7b5cefbb638b6044ca4c3f228ff859364
yun <n856839@gmail.com>
2021-01-27 20:40:26 +0800

    feat: Add login & register route & views

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
