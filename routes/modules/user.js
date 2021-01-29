5233b25d68f119baf6bcbd0c481326d03da48885
yun <n856839@gmail.com>
2021-01-29 14:25:39 +0800

    feat: add partials use connect-flash

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -5,9 +5,11 @@ const passport = require('passport')
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 // 登入
 router.get('/login', (req, res) => {
+  res.locals.error_msg = req.flash('error')
   res.render('login')
 })
 
@@ -26,10 +28,28 @@ router.get('/register', (req, res) => {
 // 註冊檢查
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符!' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: '這個 Email 已經註冊過了' })
       return res.render('register', {
+        errors,
         name,
         email,
         password,
