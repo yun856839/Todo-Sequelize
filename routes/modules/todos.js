eb33a56c0b33ca5365513433398d0c9f26301a93
yun <n856839@gmail.com>
2021-01-29 19:45:16 +0800

    feat: add update & delete todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -2,16 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const db = require('../../models')
-const { route } = require('./home')
 const Todo = db.Todo
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id
-  const UserId = req.user.id
-  return Todo.findOne({ where: { id, UserId } })
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
 
 router.get('/new', (req, res) => {
   return res.render('new')
@@ -21,6 +12,45 @@ router.post('/', (req, res) => {
   const name = req.body.name
   const UserId = req.user.id
   return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => todo.destroy())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
